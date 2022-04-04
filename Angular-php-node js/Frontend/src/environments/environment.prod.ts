dc79a8a67cec10f090dfaf34c54f079b349c1226
abdessamad-bakhadda <61042712+abdessamad-bakhadda@users.noreply.github.com>
2022-04-04 12:34:04 +0200

    Add files via upload

---
+++ Angular-php-node js/Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true ,
+  debut_url : "http://127.0.0.1:3000/" ,
+  // backend en php , debut_url :"http://localhost:63344/app_web_mobiles/Tps_me/forum-angular-php/backend/tp2/" ,
+  // backend en Node/Express ,debut_url : "http://127.0.0.1:3000/" ,
+
+};
